4a7f9c1a53bb552a0df9e4a2f71c4f93b00e54e4
natingo <nathanroger29@gmail.com>
2017-08-15 10:16:37 -0400

    first commit

--- views.py
+++ views.py
@@ -26,21 +26,22 @@ class Instruction2(Page):
 
 class Decisions(Page):
     form_model = models.Player
-    form_fields = ['gamble_{}'.format(i) for i in range(1, 11)]
+    form_fields = ['gamble_{}'.format(i)
+                   for i in range(1, Constants.num_gambles + 1)]
 
     def vars_for_template(self):
         smth = self.get_form()
-        fields_to_show=self.form_fields
+        fields_to_show = self.form_fields
         leftcol = []
         rightcol = []
-        maxlen = len(fields_to_show)+1
+        maxlen = len(fields_to_show) + 1
         for i, f in enumerate(fields_to_show):
             str1 = ', '.join(str(e) for e in [_ for _ in range(1, i+2)])
             str2 = ', '.join(str(e) for e in [_ for _ in range(i+2, maxlen )])
             leftcol.append(str1)
             rightcol.append(str2)
 
-        return {'abclist':zip(smth,leftcol,rightcol),}
+        return {'abclist': zip(smth, leftcol, rightcol), }
 
     def before_next_page(self):
         self.player.set_payoffs()
@@ -52,20 +53,17 @@ class ResultsWaitPage(WaitPage):
         pass
 
 
-class Results(Page):
-    def vars_for_template(self):
-        choice = getattr(self.player,
-                         "gamble_{}".format(self.player.gamble_num))
-
-        return {'chosen_gamble_choice': choice, }
-        pass
-
 class Results2(Page):
     def vars_for_template(self):
         choice = getattr(self.player,
                          "gamble_{}".format(self.player.gamble_num))
-
-        return {'chosen_gamble_choice': choice, }
+        maxlen = Constants.num_gambles + 1
+        str1 = ', '.join(str(e) for e in [_ for _ in range(1, self.player.gamble_num+1)])
+        str2 = ', '.join(str(e) for e in [_ for _ in range(self.player.gamble_num+1, maxlen)])
+        return {'chosen_gamble_choice': choice,
+                'str1': str1,
+                'str2': str2,
+                }
         pass
 
 
